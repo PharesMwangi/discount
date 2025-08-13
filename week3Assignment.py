92560e150c938db9433b77d4405f3a43c6f7c054
PharesMwangi <mwasmwas683@gmail.com>
2025-08-13 10:12:26 +0300

    Created a program to calculate the final price of a product if discount is given

---
+++ week3Assignment.py
@@ -0,0 +1,12 @@
+original_price = float(input("Enter the original price of the item: "))
+discount_percentage = float(input("Enter the discount percentage of the item:"))
+
+def calculate_discount(original_price, discount_percentage):
+
+    if discount_percentage > 20:
+        final_amount = original_price - (original_price * (discount_percentage / 100))
+        print(f"Pay {final_amount} for the item, you have a {discount_percentage}% discount." ) 
+    else:
+        print(f"Pay {original_price} for the item. No discount applied")
+
+calculate_discount(original_price, discount_percentage)
